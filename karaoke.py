4475acfbd77adfd720431670bbd8c47ad7b8401a
Sergio Casarrubios <scasarrubios@virtualbox.org>
2016-10-06 23:42:25 +0200

    realizado el paso de datos a json y la escritura de ellos en el fichero

--- karaoke.py
+++ karaoke.py
@@ -5,6 +5,7 @@ from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 from smallsmilhandler import SmallSMILHandler
 import sys
+import json
 
 
 if __name__ == "__main__":
@@ -21,3 +22,10 @@ if __name__ == "__main__":
             for att in line[tag]:
                 string = string + att + '="' + line[tag][att] + '"\t'
     print(string)
+    data = {}
+    nombre = str(sys.argv[1]).strip(".smil") + '.json'
+    fich = open(nombre, 'w')
+    for line in tags:
+        for tag in line:
+            data[tag] = json.dumps(line[tag])
+    json.dump(data, fich)
